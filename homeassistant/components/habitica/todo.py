1db07db3fa7a0272fc76c676a04466b11d90f001
tr4nt0r <manni@zapto.de>
2024-05-05 17:07:28 +0000

    update for DataUpdateCoordinator

--- homeassistant/components/habitica/todo.py
+++ homeassistant/components/habitica/todo.py
@@ -19,6 +19,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ServiceValidationError
 from homeassistant.helpers.entity import EntityDescription
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util import dt as dt_util
 
 from . import HabiticaConfigEntry
